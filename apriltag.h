b48b9097980a42609336b1b533c224c0f4b4aaee
Mark Grossnickle <markgrossnickle@users.noreply.github.com>
2022-06-03 15:56:38 -0600

    and again

--- apriltag.h
+++ apriltag.h
@@ -261,14 +261,16 @@ void apriltag_detector_destroy(apriltag_detector_t *td);
 // _detection_destroy and zarray_destroy yourself.
 zarray_t *apriltag_detector_detect(apriltag_detector_t *td, image_u8_t *im_orig);
 
-//
-//typedef void(*FuncCallBack)(const char* message, int size);
-//static FuncCallBack callbackInstance = nullptr;
-//DLLExport void RegisterDebugCallback(FuncCallBack cb);
+extern "C"
+{
+    //Create a callback delegate
+    /*typedef void(*FuncCallBack)(const char* message, int size);
+    static FuncCallBack callbackInstance = nullptr;
+    DLLExport void RegisterDebugCallback(FuncCallBack cb);*/
 
-// The callback signature (shared by Unity C#)`enter code here`
-typedef void(*DebugLogCallback)(const char* message, int size);
-void RegisterDebugLog(DebugLogCallback callback);
+    typedef void(*DebugLogCallback)(const char* message, int size);
+    DLLExport void RegisterDebugLog(DebugLogCallback callback);
+}
 
 // Call this method on each of the tags returned by apriltag_detector_detect
 void apriltag_detection_destroy(apriltag_detection_t *det);
